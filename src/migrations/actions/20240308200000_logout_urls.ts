591d97148fae1e3fb6e470020f7565dc94f68cd9
Jakub <kiszczyckuba20@wp.pl>
2024-03-08 18:11:29 +0100

    Feat: Added migration, which will generate default client

---
+++ src/migrations/actions/20240308200000_logout_urls.ts
@@ -0,0 +1,19 @@
+import { generateRandomName } from '../../utils';
+import type { Knex } from 'knex';
+
+export const up = async (knex: Knex): Promise<void> => {
+  return knex('oidcClients').insert({
+    client_id: 'oidcClient',
+    client_secret: generateRandomName(),
+    grant_types: ['authorization_code', 'refresh_token'],
+    scope: 'openid',
+    redirect_uris: ['http://127.0.0.1/login'],
+    post_logout_redirect_uris: ['http://127.0.0.1'],
+  });
+};
+
+export const down = async (): Promise<void> => {
+  return new Promise((resolve) => {
+    resolve();
+  });
+};
